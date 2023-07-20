8b00fe55e1cab14c3fd6653a880e79d4fd882994
bagusrizky15 <mohamad.15@students.amikom.ac.id>
2023-07-20 08:58:35 +0700

    Initial Commit

---
+++ myCard/myCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  myCardApp.swift
+//  myCard
+//
+//  Created by bagus on 20/07/23.
+//
+
+import SwiftUI
+
+@main
+struct myCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
